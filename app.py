8077d4b177d0177dcf8b8827f33c09f7d7be6467
mvram123 <mramarao923@gmail.com>
2021-08-26 13:01:07 +0530

    code update

---
+++ app.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import numpy as np
+import streamlit as st
+from PIL import Image
+import requests
+from io import BytesIO
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title("Pnuemonia Detection Image Classifier")
+st.text("Provide URL of Chest Xray for Pneumonia Detection")
+
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('models/model.h5')
+    return model
+
+
+with st.spinner('Loading Model Into Memory....'):
+  model = load_model()
+
+classes = ['Pneumonia', 'Healthy']
+
+
+def decode_img(image):
+    img = tf.image.decode_jpeg(image, channels=3)
+    img = tf.image.resize(img, [224, 224])
+    return np.expand_dims(img, axis=0)
+
+
+path = st.text_input('Enter Image URL to Classify.. ',
+                     'https://raw.githubusercontent.com/mvram123/mvram123/main/p4.jpeg')
+if path is not None:
+    content = requests.get(path).content
+
+    st.write("Predicted Class :")
+    with st.spinner('classifying.....'):
+        label = np.argmax(model.predict(decode_img(content)), axis=1)
+    print(label)
+    st.write(classes[label[0]])
+    st.write("")
+    image = Image.open(BytesIO(content))
+    st.image(image, caption='Pneumonia Detection', use_column_width=True)
+
